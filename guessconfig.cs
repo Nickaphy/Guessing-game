d543f1005e78062facc43b48b3253869d472a46b
Nickaphy <nickdahchris@gmail.com>
2024-11-24 00:06:25 +0100

    fixed config and passing of difficulty

--- guessconfig.cs
+++ guessconfig.cs
@@ -6,17 +6,20 @@ namespace Program //namespace for multifile
 
     class Start //main class
     {
-
+        public static int difficultychoice;
         static void Main(string[] args) //main method
         {
-            welcome();
+            difficultychoice = welcome(); //get difficulty choice from welcome()
+            //Calling the StartGame class to game and passing it difficultychoice
+            StartGame game = new StartGame(difficultychoice);
+            //starts the Start method
+            game.Start();
         }
 
         static int welcome() //welcome method
         {
             Console.WriteLine("Welcome to the Guessing Game!\nChoose a diffiulty level: \n[1] Easy\n[2] Medium\n[3] Hard");
             string input = Console.ReadLine(); //reading user input
-            int difficultychoice; //declaring an empty int
             if (int.TryParse(input, out difficultychoice)) //try to parse the input to an int and output as difficultychoice
             {
                 switch (difficultychoice) //switch statements for the difficulty choice
--- guessgame.cs
+++ guessgame.cs
@@ -2,12 +2,18 @@ using System;
 
 namespace Program
 {
-
-    public class Game
+    public class StartGame
     {
-        public static void Easy()
+        private int difficulty;
+        //this is a constructor which is not a method used to initialize an instance of this class
+        public StartGame(int difficultychoice)
         {
+            difficulty = difficultychoice;
+        }
 
+        public void Start()
+        {
+            //game logic goes here
         }
     }
 }
